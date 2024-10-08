283f72822a49b42466814dd82e8affd83c58961a
unknown <althaf.umer@strategic.ae>
2024-10-08 18:45:20 +0400

    updated styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
       <Head>
 
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
         <HeaderComponent />
         <link rel="preconnect" href="https://deliver.kontent.ai" crossOrigin="anonymous" />
